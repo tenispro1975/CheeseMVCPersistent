4636d8d16b67d42d3b6e6d721198d365aca26d01
Steven Palmer <steven.palmer1975@gmail.com>
2017-12-21 22:24:08 -0600

    error on adding item to menu

--- src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
+++ src/CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -23,6 +23,8 @@ namespace CheeseMVC.ViewModels
 
         public List<SelectListItem> Categories { get; set; }
 
+        public AddCheeseViewModel() { }
+
         public AddCheeseViewModel(IEnumerable<CheeseCategory> categories)
         {
 
